2276055e77fe7d05ae9c0f67846baac674815c57
arother65 <anro@email.de>
2026-01-04 17:54:33 +0100

    ...added checks for used rows.

--- src/pages/VierGewinnt.jsx
+++ src/pages/VierGewinnt.jsx
@@ -22,168 +22,6 @@ import Footer from '../components/Footer'
 //
 export default function VierGewinnt() {
 
-  // "hook"-functions
-  const [noCoins, setNoCoins] = useState(4)  // used in Array.map() for #coins
-  const [coinValue, setCoinValue] = useState(1)
-  const [dragSourceId, setDragSourceId] = useState(null)
-
-  // creates a fn of type NavigateFunction
-  const fnNavigate = useNavigate()
-
-  // 
-  const getRandomCoinValue = () => {
-    let actCoinValue = 1
-
-    actCoinValue = Math.random() * 5
-
-    while (actCoinValue === 0 || actCoinValue === undefined || actCoinValue < 1.0) {
-      actCoinValue = Math.random() * 5
-    }  //
-
-    actCoinValue = Math.floor(actCoinValue)
-    console.log('actCoinValue: ', actCoinValue)
-
-    // checking actCoinValue to fit in the usual EUR-coin-values (1, 2, 5)
-    if (actCoinValue === 1 || actCoinValue === 2) { return actCoinValue }
-    if (actCoinValue > 2 && actCoinValue <= 5) { return 5 }
-    if (actCoinValue > 5) { return 5 }
-  }  // getRandomCoinValue()
-
-  // sets coinValue when view is first created
-  useEffect(() => {
-    const newCoinValue = getRandomCoinValue()
-    setCoinValue(newCoinValue)  // overrides initial value
-    console.log('useEffect on coinValue')
-  }, [coinValue])
-
-  // gets value from select and creates random values for the coins
-  const handleNoObjects = async (e) => {
-
-    // setting new value for coinValue
-    console.log('coinValue before: ', coinValue)
-    setCoinValue(getRandomCoinValue())
-    console.log('coinValue after: ', coinValue)
-
-    // err: new no. of coins : invalid state usage 
-    console.log('No. before', noCoins)
-    setNoCoins(e.target.value)
-    console.log('No. after', e.target.value)
-  }  // handleNoObjects()
-
-  useEffect(() => {
-    console.log('useEffect on noCoins')
-  }, [noCoins])
-
-  //* drag&drop handling
-  async function fnOnDrop(event) {
-    event.preventDefault()
-
-    // mark actual row/col-combination in playGround as USED, event.currentTarget.id = actual dropTarget  
-    console.log('got ID as dropTarget: ', event.currentTarget.id)
-    let splittedID = event.currentTarget.id.split('-')
-    let rowId = splittedID[0]
-    let colId = splittedID[1]
-
-    // check if the actual coin () is already in playGround 
-    let parentDiv, checkedCell, actCol
-    for (let i = 0; i < 4; i++) {
-      actCol = `col${i}`
-      checkedCell = playGround[`row${rowId}`][actCol]
-
-      // coin aus der aktuell untersuchten cell 'checkedCell' entfernen:
-      if (checkedCell.used === true) {
-        checkedCell.used = false
-        checkedCell.coinId = null
-
-        // get that div with css-class col
-        parentDiv = document.getElementById(`${rowId}-${i}`)
-        console.log(parentDiv.className)
-        parentDiv.className = parentDiv.className.replace('bg-success', '')
-      }
-    }  // checking if cell is already in use 
-
-    // check if this row-col in playGround is already in use
-    let cell = playGround[`row${rowId}`][`col${colId}`]
-
-    // check if row is already full
-    const row = playGround[`row${rowId}`]
-
-    if (cell.used === false) {
-      // mark this cell
-      cell.used = true
-      cell.coinId = dragSourceId
-
-      // console.log(playGround)
-      console.log('dragSourceId', dragSourceId)
-
-      // add new node
-      let data = event.dataTransfer.getData("text/plain")
-      event.target.appendChild(document.getElementById(data))
-      //? removeChild() aus dem PLayer-Bereich, der dragSource
-
-      // color the cell in use
-      let className = event.currentTarget.className
-      event.currentTarget.className = className + ' bg-success'
-
-      // mark as used
-      setPlayground(playGround)
-
-      if (row.col0.used === true &&
-        row.col1.used === true &&
-        row.col2.used === true &&
-        row.col3.used === true
-      ) {
-        console.log('row full! ID: ', rowId)
-        event.stopPropagation()
-        return
-      }
-
-    } else {
-      // cell was already used 
-      event.stopPropagation()
-      return
-    }
-  }  // fnOnDrop(event) 
-
-  async function fnDragStart(event) {
-    console.log('fnDragStart', event.target.id)
-    //? write actualCoin to global memory
-    setDragSourceId(event.target.id)
-
-    event.currentTarget.style.opacity = "0.25"
-
-    event.dataTransfer.setData("text/plain", event.target.id)  // text/html || text/plain
-  }  // fnDragStart()
-
-  async function fnAllowDrop(event) {
-    event.preventDefault()
-
-    // console.log('fnAllowDrop', event)
-    event.dataTransfer.effectAllowed = 'none'
-    event.dataTransfer.dropEffect = 'copy'  // move
-  }  // fnAllowDrop() / event onDragOver
-
-  // custom state and fn's to handle <Menu />
-  const [anchorEl, setAnchorEl] = useState(null)
-  const open = Boolean(anchorEl)
-  const handleClick = (event) => { setAnchorEl(event.currentTarget) }
-
-  const handleClose = (event) => {
-    setAnchorEl(null)
-    handleNoObjects(event)  // current MenuItem in: event.target.value
-
-    // setNoCoins(event.target.value)
-  }
-
-  // common sx-object for all <MenuItem />'s used
-  const menuItemSx = {
-    '&:hover': {
-      color: 'white',
-      // backgroundColor: 'olivedrab', 
-      backgroundColor: 'primary.light',
-    }
-  }  // 
-
   // Aufbau des Spielfeldes, Verwaltung der bereits besetzten Positionen in "fnOnDrop()"
   let playGroundInit = {
     row0: {
@@ -258,12 +96,16 @@ export default function VierGewinnt() {
         coinId: null
       }
     }
-  }  // playGround
-  const [playGround, setPlayground] = useState(playGroundInit)
+  }  // playGroundInit
 
+  // "hook"-functions
+  const [playGround, setPlayground] = useState(playGroundInit)
+  /*   useEffect(() => {
+      console.log('new values for playGround...')
+    }, [playGround])  // useEffect() for playGround 
+   */
   // useRef for the Coins created with <Avatar />-Component 
   const avatarRef = useRef(null)
-
   useEffect(() => {
     const el = avatarRef.current;
     if (!el) return;
@@ -272,8 +114,183 @@ export default function VierGewinnt() {
     console.log("width:", el.offsetWidth);       // number
     console.log("height:", el.offsetHeight);     // number
     // console.log("", el.data-coin-value)  // errs 
-
   }, [])  // useeffect() for avatarRef
+
+  const [noCoins, setNoCoins] = useState(4)  // used in Array.map() for #coins
+  const [dragSourceId, setDragSourceId] = useState(null)
+  useEffect(() => {
+    console.log('useEffect: ')
+  }, [noCoins, dragSourceId, playGround])  //?
+
+  // sets coinValue when view is first created
+  const [coinValue, setCoinValue] = useState(1)
+  useEffect(() => {
+    const newCoinValue = getRandomCoinValue()
+    setCoinValue(newCoinValue)  // overrides initial value
+    console.log('useEffect on coinValue')
+  }, [coinValue])
+
+  // gets value from select and creates random values for the coins
+  const handleNoObjects = async (e) => {
+
+    // setting new value for coinValue
+    console.log('coinValue before: ', coinValue)
+    setCoinValue(getRandomCoinValue())
+    console.log('coinValue after: ', coinValue)
+
+    // err: new no. of coins : invalid state usage 
+    console.log('No. before', noCoins)
+    setNoCoins(e.target.value)
+    console.log('No. after', e.target.value)
+  }  // handleNoObjects()
+
+  // creates a fn of type NavigateFunction
+  const fnNavigate = useNavigate()
+
+  // custom state and fn's to handle <Menu />
+  const [anchorEl, setAnchorEl] = useState(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event) => { setAnchorEl(event.currentTarget) }
+
+  const handleClose = (event) => {
+    setAnchorEl(null)
+    handleNoObjects(event)  // current MenuItem in: event.target.value
+
+    // setNoCoins(event.target.value)
+  }
+
+  // common sx-object for all <MenuItem />'s used
+  const menuItemSx = {
+    '&:hover': {
+      color: 'white',
+      // backgroundColor: 'olivedrab', 
+      backgroundColor: 'primary.light',
+    }
+  }  // 
+
+  // 
+  const getRandomCoinValue = () => {
+    let actCoinValue = 1
+
+    actCoinValue = Math.random() * 5
+
+    while (actCoinValue === 0 || actCoinValue === undefined || actCoinValue < 1.0) {
+      actCoinValue = Math.random() * 5
+    }  //
+
+    actCoinValue = Math.floor(actCoinValue)
+    console.log('actCoinValue: ', actCoinValue)
+
+    // checking actCoinValue to fit in the usual EUR-coin-values (1, 2, 5)
+    if (actCoinValue === 1 || actCoinValue === 2) { return actCoinValue }
+    if (actCoinValue > 2 && actCoinValue <= 5) { return 5 }
+    if (actCoinValue > 5) { return 5 }
+  }  // getRandomCoinValue()
+
+  //* drag&drop handling
+  function fnOnDrop(event) {
+    event.preventDefault()
+
+    // mark actual row/col-combination in playGround as USED, event.currentTarget.id = actual dropTarget  
+    console.log('got ID as dropTarget: ', event.currentTarget.id)
+    let splittedID = event.currentTarget.id.split('-')
+    let rowId = splittedID[0]
+    let colId = splittedID[1]
+
+    // check if this row-col in playGround is already in use
+    let cell = playGround[`row${rowId}`][`col${colId}`]
+    if (cell.used === true) {
+      // cell was already used
+      event.stopPropagation()
+      return
+    }
+
+    // check if the actual coin () is already in ONE row playGround 
+    //! Unterscheidung je nach event: drop aus dieser Zelle ODER drop in diese Zelle
+    let parentDiv, checkedCell, actCol
+    // check all rows 
+    for (let rowIndex = 0; rowIndex < 4; rowIndex++) {
+      // console.log(rowIndex)
+
+      // check all cols in this row 
+      for (let colIndex = 0; colIndex < 4; colIndex++) {
+        console.log('row: ', rowIndex, 'col: ', colIndex)
+
+        actCol = `col${colIndex}`
+        checkedCell = playGround[`row${rowIndex}`][actCol]
+
+        // coin aus der aktuell untersuchten cell 'checkedCell' entfernen:
+        if (checkedCell.used === true) {
+          checkedCell.used = false
+          checkedCell.coinId = null
+
+          // get that div with css-class col
+          parentDiv = document.getElementById(`${rowIndex}-${colIndex}`)
+          console.log(parentDiv.className)
+          parentDiv.className = parentDiv.className.replace('bg-success', '')
+        }
+      }  // checking if cell is already in use 
+    }  // run through all rows 
+
+    // check if row is already full
+    const row = playGround[`row${rowId}`]
+
+    if (cell.used === false) {
+      // mark this cell as used & memorize the coin just dropped in here 
+      cell.used = true
+      cell.coinId = dragSourceId
+
+      // add this coin to a cell in the playground 
+      let data = event.dataTransfer.getData("text/plain")
+      event.target.appendChild(document.getElementById(data))
+
+      // restore the coin's opacity 
+      let actCoin = document.getElementById(dragSourceId)
+      actCoin.style.opacity = 1
+
+      //? removeChild() aus dem Player-Bereich, der dragSource
+
+      // color the cell in use
+      let className = event.currentTarget.className
+      event.currentTarget.className = className + ' bg-success'
+
+      // mark as used
+      setPlayground(playGround)
+
+      if (row.col0.used === true &&
+        row.col1.used === true &&
+        row.col2.used === true &&
+        row.col3.used === true
+      ) {
+        console.log('row full! ID: ', rowId)
+        event.stopPropagation()
+        return
+      }
+
+    } else {
+      // cell was already used 
+      event.stopPropagation()
+      return
+    }
+  }  // fnOnDrop(event) 
+
+  function fnDragStart(event) {
+    // console.log('fnDragStart', event.target.id)
+
+    // write the actual Coin to global memory:
+    setDragSourceId(event.target.id)
+
+    event.currentTarget.style.opacity = "0.45"
+    event.dataTransfer.setData("text/plain", event.target.id)  // text/html || text/plain
+  }  // fnDragStart()
+
+  function fnAllowDrop(event) {
+    event.preventDefault()
+
+    // console.log('fnAllowDrop', event)
+    event.dataTransfer.effectAllowed = 'none'
+    event.dataTransfer.dropEffect = 'copy'  // move
+  }  // fnAllowDrop() / event onDragOver
 
   // creating the view:
   return (
