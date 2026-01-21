b325f9c8ace496845cab3fd30c2585473842fe09
arother65 <anro@email.de>
2026-01-21 22:09:00 +0100

    ...more work on backgrounds.

--- src/pages/Mario3D.jsx
+++ src/pages/Mario3D.jsx
@@ -11,11 +11,15 @@ import { useNavigate } from 'react-router-dom'
 import { AppBar, IconButton, Toolbar, Tooltip } from '@mui/material'
 import HomeIcon from '@mui/icons-material/Home'
 
+// 
+import * as THREE from 'three'
+import { useRef, useMemo } from 'react'
+import { useFrame } from '@react-three/fiber'
+
+// import logo192 from '../components/logo192.png'
+
+
 //
-// import { useMemo } from "react";
-// import { useFrame } from "@react-three/fiber";
-
-
 function MetalRod({ args, position, rotation, color }) {
    return (
       <mesh position={position} rotation={rotation}>
@@ -35,9 +39,73 @@ function MetalRod({ args, position, rotation, color }) {
    )
 }  // MetalRod()
 
+function createSkeleton() {
+   const root = new THREE.Bone()
+   root.position.y = 0
+
+   const head = new THREE.Bone()
+   head.position.y = 1
+   root.add(head)
+
+   return new THREE.Skeleton([root, head])
+}
+
+function createSkinnedGeometry() {
+   const geo = new THREE.BoxGeometry(0.5, 2, 0.5)
+   const pos = geo.attributes.position
+
+   const skinIndices = []
+   const skinWeights = []
+
+   for (let i = 0; i < pos.count; i++) {
+      const y = pos.getY(i)
+
+      if (y > 0) {
+         skinIndices.push(1, 0, 0, 0) // head bone
+         skinWeights.push(1, 0, 0, 0)
+      } else {
+         skinIndices.push(0, 0, 0, 0) // root bone
+         skinWeights.push(1, 0, 0, 0)
+      }
+   }
+
+   geo.setAttribute(
+      'skinIndex',
+      new THREE.Uint16BufferAttribute(skinIndices, 4)
+   )
+   geo.setAttribute(
+      'skinWeight',
+      new THREE.Float32BufferAttribute(skinWeights, 4)
+   )
+
+   return geo
+}
+
+function SkinnedCharacter() {
+   const skeleton = useMemo(createSkeleton, [])
+   const geometry = useMemo(createSkinnedGeometry, [])
+
+   useFrame(({ clock }) => {
+      skeleton.bones[1].rotation.z = Math.sin(clock.elapsedTime) * 0.5
+   })
+
+   return (
+      <group position={[0, -1, 0]}>
+         <skinnedMesh geometry={geometry} skeleton={skeleton}>
+            <meshStandardMaterial color="orange" skinning />
+         </skinnedMesh>
+
+         {/* VERY IMPORTANT */}
+         <primitive object={skeleton.bones[0]} />
+      </group>
+   )
+}
+
+
+
 function Mario() {
    return (
-      <group position={[0, 1, 0]}>
+      <group position={[0, -0.05, 0]}>
          {/* Head */}
          <mesh position={[0, 2.2, 0]}>
             <sphereGeometry args={[0.6, 32, 32]} />
@@ -53,7 +121,8 @@ function Mario() {
          {/* Body */}
          <mesh position={[0, 1.3, 0]}>
             <cylinderGeometry args={[0.5, 0.6, 1.2, 32]} />
-            <meshStandardMaterial color="blue" />
+            <meshStandardMaterial skinning
+               color="#e52521" />
          </mesh>
 
          {/* Arms */}
@@ -80,7 +149,34 @@ function Mario() {
 }  // Mario()
 
 
-// Car3D page component
+//
+function AnimatedTerrain() {
+  const ref = useRef()
+
+  useFrame(({ clock }) => {
+    const pos = ref.current.geometry.attributes.position
+    const t = clock.elapsedTime
+
+    for (let i = 0; i < pos.count; i++) {
+      const x = pos.getX(i)
+      const y = pos.getY(i)
+      pos.setZ(i, Math.sin(x * 0.2 + t) * Math.cos(y * 0.2) * 2)
+    }
+
+    pos.needsUpdate = true
+    ref.current.geometry.computeVertexNormals()
+  })
+
+  return (
+    <mesh ref={ref} rotation={[-Math.PI / 2, 0, 0]}>
+      <planeGeometry args={[50, 50, 128, 128]} />
+      <meshStandardMaterial color="#2196f3" />
+    </mesh>
+  )
+}  // AnimatedTerrain()
+
+
+// Mario3D page component
 export default function Mario3D() {
 
    const fnNavigate = useNavigate()  // creates a fn of type NavigateFunction
@@ -123,11 +219,15 @@ export default function Mario3D() {
 
                <Mario />
 
+               {/* <SkinnedCharacter /> */}
+
+               <AnimatedTerrain />
+
                {/* Ground */}
-               <mesh position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
-                  <planeGeometry args={[20, 20]} />
-                  <meshStandardMaterial color="olive" />
-               </mesh>
+               {/* <mesh position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
+                  <planeGeometry args={[20, 20, 10, 10]} />
+                  <meshStandardMaterial color="grey" /> */}
+               {/* </mesh> */}
 
                <OrbitControls />
             </Canvas>
